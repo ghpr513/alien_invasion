36fc351b26a2ea8c8b458d9c85aa0d2deabbe550
ghpr513 <1981563760@qq.com>
2025-03-16 16:07:27 +0800

    取消全屏显示

--- setting.py
+++ setting.py
@@ -13,7 +13,7 @@ class Settings:
         self.ship_speed = 3.5
 
         # 子弹设置
-        self.bullet_speed == 2.0
+        """self.bullet_speed == 2.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (60, 60, 60)"""
