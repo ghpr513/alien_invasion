333fa0077d728a65c16e3ac6ba379ec4d9179a93
ghpr513 <1981563760@qq.com>
2025-03-16 19:34:46 +0800

    创建 Alien 实例，注释了 alien_invasion.py 中关于限制子弹数量的部分

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from setting import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -20,6 +21,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._creat_fleet()
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -60,26 +64,33 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """创建一颗子弹，并将其加入编组 bullets"""
-        if len(self.bullets) < self.settings.bullet_allowed:
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
+        #if len(self.bullets) < self.settings.bullet_allowed:
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_bullets(self):
         """更新子弹的位置并删除已消失的子弹"""
-        #更新子弹位置
+        # 更新子弹位置
         self.bullets.update()
 
-        #删除已消失的子弹
+        # 删除已消失的子弹
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _creat_fleet(self):
+        """创建一个外星舰队"""
+        #创建一个外星人
+        alien = Alien(self)
+        self.aliens.add(alien)
+    
     def _update_screen(self):
         """更新屏幕上的图像，并且切换到新屏幕"""
         self.screen.fill(self.settings.bg_color)
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
