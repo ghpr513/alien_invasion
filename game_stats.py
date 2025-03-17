36665530a6152f68d0c435668f1b551955592bc5
ghpr513 <1981563760@qq.com>
2025-03-17 15:23:56 +0800

    最高分

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # 在任何情况下都不应该重置最高分
+        self.high_score = 0
+
     def reset_stats(self):
         """初始化游戏运行期间可能变化的统计信息"""
         self.ship_left = self.settings.ship_limit
