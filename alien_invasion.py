43c03a642fa87564e441bf923d5a9c906d430e10
ghpr513 <1981563760@qq.com>
2025-03-17 15:02:46 +0800

    提高分数

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,7 +132,7 @@ class AlienInvasion:
         
         if collisions:
             for aliens in collisions.values():
-                self.stats.score += self.settings.alien_points * len*(aliens)
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
         
         if not self.aliens:
--- setting.py
+++ setting.py
@@ -23,6 +23,9 @@ class Settings:
         # 以什么速度加快游戏的节奏
         self.speedup_scale = 1.2
 
+        #外星人分数的提高速度
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -38,9 +41,11 @@ class Settings:
         self.fleet_direction = 1
 
     def increase_speed(self):
-        """提高速度设置的值"""
+        """提高速度设置的值和外星人分数"""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
 
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
     
